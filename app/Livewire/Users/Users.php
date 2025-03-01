c804ae348964cee5cb2f8cc9c8844ca6c8fab738
Rochmad Arifin <arifinwhite@gmail.com>
2025-03-02 05:47:42 +0800

    add seeder admin@example.com password

--- app/Livewire/Users/Users.php
+++ app/Livewire/Users/Users.php
@@ -2,14 +2,20 @@
 
 namespace App\Livewire\Users;
 
+use App\Models\User;
 use Livewire\Attributes\Title;
 use Livewire\Component;
+use Livewire\WithoutUrlPagination;
+use Livewire\WithPagination;
 
 class Users extends Component
 {
     #[Title('Users')]
+    public $search = '';
+    use WithPagination, WithoutUrlPagination;
     public function render()
     {
-        return view('livewire.notfound');
+        $users = User::where('name', 'like', '%' . $this->search . '%')->paginate(10);
+        return view('livewire.users.users', compact('users'));
     }
 }
--- resources/views/livewire/users/users.blade.php
+++ resources/views/livewire/users/users.blade.php
@@ -1,3 +1,36 @@
 <div>
-    {{-- If you look to others for fulfillment, you will never truly be fulfilled. --}}
+    <div class="row mb-3">
+        <div class="col-md-3">
+            <input type="text"class="form-control" placeholder="Search" livewire:model.live="search">
+        </div>
+    </div>
+    <div class="table table-responsive">
+        <table class="table table-striped">
+            <thead>
+                <tr>
+                    <th style="width: 10px">#</th>
+                    <th>Name</th>
+                    <th>Email</th>
+                    <th>Progress</th>
+                    <th style="width: 40px">Label</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($users as $user)
+                    <tr>
+                        <td>{{ $user->id }}</td>
+                        <td>{{ $user->name }}</td>
+                        <td>{{ $user->email }}</td>
+                        <td>
+                            <div class="progress progress-xs">
+                                <div class="progress-bar progress-bar-danger" style="width: 55%"></div>
+                            </div>
+                        </td>
+                        <td><span class="badge bg-danger">65%</span></td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+    {{ $users->links() }}
 </div>
